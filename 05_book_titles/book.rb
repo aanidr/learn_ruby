f3ad389c033df36c6c2a16b42125f69a6bc84b24
Adrian Nawrocki <adrian.nawrocki@netguru.co>
2019-12-11 13:42:10 +0100

    task 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
 # write your code here
+  attr_reader :title
+
+  def title=(new_title)
+    @title = titleize(new_title)
+  end
+end
+
+def titleize(msg)
+  little_words = %w[the over in of and a an]
+  msg.capitalize.split.map do |word|
+    little_words.include?(word) ? word : word.capitalize
+  end.join(' ')
 end
