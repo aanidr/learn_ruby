234e5ffaec895fe353f4a7904b2426c2fcedd889
Adrian Nawrocki <adrian.nawrocki@netguru.co>
2019-12-11 11:47:20 +0100

    task 2

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,25 @@
 #write your code here
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  array.inject(0) { |s, x| add(s, x) }
+end
+
+def multiply(array)
+  array.inject(1) { |m, x| m * x }
+end
+
+def power(a, b)
+  a**b
+end
+
+def factorial(num)
+  (1..num).inject(1) { |f, x| f * x }
+end
