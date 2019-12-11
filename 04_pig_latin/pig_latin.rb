8225774a40adf6204f5b0505c741335474e5483d
Adrian Nawrocki <adrian.nawrocki@netguru.co>
2019-12-11 13:17:18 +0100

    task 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
 #write your code here
+
+def translate(msg)
+  msg.split.map { |word| piggify(word) }.join(' ')
+end
+
+def piggify(word)
+  vowels = %w[a e i o u]
+  until vowels.include?(word[0]) do
+    word = word[1..-1] + word[0]
+  end
+  word + 'ay'
+end
