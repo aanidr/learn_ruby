7f890d0bf39e68dfc0cf5635b081681a95430d22
Adrian Nawrocki <adrian.nawrocki@netguru.co>
2019-12-11 13:42:15 +0100

    task 6

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,29 @@
 class Timer
   #write your code here
+
+  def seconds
+    @seconds || 0
+  end
+
+  def minutes
+    @minutes || 0
+  end
+
+  def hours
+    @hours || 0
+  end
+
+  def seconds=(new_sec)
+    @seconds = new_sec % 60
+    normalize_minutes(new_sec / 60)
+  end
+
+  def normalize_minutes(new_min)
+    @minutes = new_min % 60
+    @hours = new_min / 60
+  end
+
+  def time_string
+    Time.new(1900,1,1,hours,minutes,seconds).strftime('%T')
+  end
 end
